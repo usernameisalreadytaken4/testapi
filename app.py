a9198621fdfeda818f9e10222edd2e4c491983b2
admin <romul1988@gmail.com>
2017-09-22 16:16:52 +0000

    second commit

--- app.py
+++ app.py
@@ -1,31 +1,106 @@
-from flask import Flask, jsonify, abort
+#!/usr/bin/env python
+from flask import Flask, jsonify, abort, make_response, request, url_for
+from flask_httpauth import HTTPBasicAuth
 
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+
+@auth.get_password
+def get_password(username):
+    if username == 'miguel':
+        return 'python'
+    return None
+
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 403)
+
+
+def make_public_task(task):
+    new_task = {}
+    print(task)
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_tasks', task_id=task['id'], _external=True)
+        else:
+            new_task[field] = task[field]
+    return new_task
+
 
 tasks = [
     {
         'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Chees, Pizza, Fruit, Tylenol',
+        'title': 'Buy groceries',
+        'description': 'Milk, Chees, Pizza, Fruit, Tylenol',
         'done': False
     },
     {
-        'id':2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good python tutorial on the web',
+        'id': 2,
+        'title': 'Learn Python',
+        'description': 'Need to find a good python tutorial on the web',
         'done': False
     }
 ]
 
 
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
-def get_tasks(task_id):
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@auth.login_required
+def get_tasks():
+    return jsonify({'tasks': list(map(make_public_task, tasks))})
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+@auth.login_required
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description',''),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}, 201)
+
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+@auth.login_required
+def update_task(task_id):
     task = list(filter(lambda t: t['id'] == task_id, tasks))
-    if task is None:
+    if len(task) == 0:
         abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) != str:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not str:
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
     return jsonify({'task': task[0]})
 
 
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+@auth.login_required
+def delete_task(task_id):
+    task = list(filter(lambda t: t['id'] == task_id, tasks))
+    if len(task) == 0:
+        abort(404)
+    task.remove(task[0])
+    return jsonify({'result': True})
+
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=5005)
